abeed3e822e3f5fd0586bbe5266325e5eddda9a0
Pedro Henrique Dias de Souza <desenvolvedorweb7070@hotmail.com>
2026-05-07 23:21:28 -0300

    Update function.js

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,10 +1,12 @@
 const mainDrink = document.getElementById("main-drink");
 const thumbs = document.querySelectorAll(".thumb-item");
+const mainDrinkMobile = document.getElementById("main-drink-mobile");
 
 thumbs.forEach(function(thumb){
     thumb.addEventListener("click",function(){
         const newImage = this.getAttribute('data-img');
         mainDrink.src = newImage;
+        mainDrinkMobile.src = newImage;
 
         thumbs.forEach(function(t){
             t.classList.remove('active-thumb');
